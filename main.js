837bad65c98b061644b3fd47a11834ab2928515d
Siddheshh9 <109347253+Siddheshh9@users.noreply.github.com>
2022-10-23 12:50:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+x = 0;
+y = 0;
+
+function preload() {
+    Mustache = loadImage("https://i.postimg.cc/1tf5Lm7f/Mustache-removebg-preview.png")
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPose);
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    fill(0, 25, 25);
+    stroke(0, 25, 25);
+    image(Mustache, x - 22, y - 5, 50, 80);
+}
+
+function take_snapshot() {
+    save("my_picture.png");
+}
+
+function modelLoaded() {
+    console.log("PoseNet Is Initialized");
+}
+
+function gotPose(results) {
+    if (results.length > 0) {
+        console.log(results);
+        console.log("nose x: " + x);
+        console.log("nose y: " + y);
+        x = results[0].pose.nose.x
+        y = results[0].pose.nose.y
+    };
+}
